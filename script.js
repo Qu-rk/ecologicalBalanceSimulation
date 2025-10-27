85f40b87900852018897b358ef10c2d1e4527e17
Qu-rk <124342210+Qu-rk@users.noreply.github.com>
2025-10-27 22:42:51 +0900

    Refactor comments and update calculations in script.js

--- script.js
+++ script.js
@@ -2,24 +2,34 @@ document.addEventListener('DOMContentLoaded', () => {
     const pro = document.getElementById("pro");
     const con1 = document.getElementById("con1");
     const con2 = document.getElementById("con2");
+    const pro_t = document.getElementById("pro_t");
+    const con1_t = document.getElementById("con1_t");
+    const con2_t = document.getElementById("con2_t");
+
 
     setInterval(() => {
 
-        //생산자가 번식한다
+        // 생산자가 번식한다
         pro.value = Number(pro.value) * 1.25;
 
-        //1차 소비자가 생산자를 먹고 번식한다
-        pro.value = Number(pro.value) - Number(con1.value) * 0.15;
-        con1.value = Number(con1.value) + Number(pro.value) * 0.15;
+        // 1차 소비자가 생산자를 먹고 번식한다
+        pro.value = Number(pro.value) - Number(con1.value) * 0.2;
+        con1.value = Number(con1.value) + Number(con1.value) * Number(pro.value) / 7500;
 
-        //2차 소비자가 1차 소비자를 먹고 번식한다
-        con1.value = Number(con1.value) - Number(con2.value) * 0.15;
-        con2.value = Number(con2.value) + Number(con1.value) * 0.15;
+        // 2차 소비자가 1차 소비자를 먹고 번식한다
+        con1.value = Number(con1.value) - Number(con2.value) * 0.2;
+        con2.value = Number(con2.value) + Number(con1.value) * Number(con2.value) / 7500;
 
-        //자연히 죽는다
+        // 자연히 죽는다
         pro.value = Number(pro.value) * 0.9;
         con1.value = Number(con1.value) * 0.9;
-        con2.value = Number(con2.value) * 0.8;
+        con2.value = Number(con2.value) * 0.9;
+
+        // 숫자 표시
+        pro_t.innerHTML = pro.value
+        con1_t.innerHTML = con1.value
+        con2_t.innerHTML = con2.value
 
     }, 30);
 });
+
